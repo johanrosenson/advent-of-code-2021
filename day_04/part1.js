2028e9bd136023e3d5546f9e586861ddfd481162
Johan <johan@devlop.se>
2021-12-04 12:55:45 +0700

    day4

---
+++ day_04/part1.js
@@ -0,0 +1,111 @@
+require('../array');
+
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf8')
+    .split('\n')
+    // .map((input) => parseInt(input))
+
+const numbers = input.shift().split(',').map(n=>parseInt(n));
+
+const boards = [];
+
+const parseBoardRow = function (row) {
+    return row.split(/\s+/g)
+        .filter(function (cell) {
+            return cell !== '';
+        })
+        .map(function (cell) {
+            return parseInt(cell);
+        });
+};
+
+for (let i = 0; i < input.length; i+=6) {
+    const board = [
+        parseBoardRow(input[i+1]),
+        parseBoardRow(input[i+2]),
+        parseBoardRow(input[i+3]),
+        parseBoardRow(input[i+4]),
+        parseBoardRow(input[i+5]),
+    ];
+
+    boards.push(board);
+}
+
+const checkNumbers = function (numbersDrawn, numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (! numbersDrawn.includes(numbers[i])) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+const isWinner = function (numbersDrawn, board) {
+    // check rows
+    for (let i = 0; i < 5; i++) {
+        const numbers = board[i];
+
+        if (checkNumbers(numbersDrawn, numbers)) {
+            return true;
+        }
+    }
+
+    // check columns
+    for (let i = 0; i < 5; i++) {
+        const numbers = [
+            board[0][i],
+            board[1][i],
+            board[2][i],
+            board[3][i],
+            board[4][i],
+        ];
+
+        if (checkNumbers(numbersDrawn, numbers)) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+const findWinningBoard = function (numbersDrawn, boards) {
+    return boards.find(function (board) {
+        return isWinner(numbersDrawn, board);
+    });
+};
+
+const calculateScore = function (board, numbersDrawn, lastNumberDrawn) {
+    const unmarkedNumbers = [
+        ...board[0],
+        ...board[1],
+        ...board[2],
+        ...board[3],
+        ...board[4],
+    ].filter(function (number) {
+        return ! numbersDrawn.includes(number);
+    });
+
+    return unmarkedNumbers.sum() * lastNumberDrawn;
+};
+
+let numbersDrawn = [];
+
+// play the numbers
+for (let i = 0; i < numbers.length; i++) {
+    const numberDrawn = numbers[i];
+
+    numbersDrawn.push(numbers[i]);
+
+    let winningBoard = findWinningBoard(numbersDrawn, boards);
+
+    if (winningBoard) {
+        const score = calculateScore(winningBoard, numbersDrawn, numberDrawn);
+
+        console.log(score);
+        return;
+    }
+}
+
+console.log('no winner');
