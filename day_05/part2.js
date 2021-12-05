12bdf20d5d41b26b7c6ec683e07fd53a41f40ac7
Johan <johan@devlop.se>
2021-12-05 12:40:46 +0700

    day5

---
+++ day_05/part2.js
@@ -0,0 +1,84 @@
+require('../array');
+
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf8')
+    .split('\n')
+
+const grid = [];
+
+input.forEach(function (line) {
+    const [start, end] = line.split(' -> ');
+    const [x1, y1] = start.split(',').map(n=>parseInt(n));
+    const [x2, y2] = end.split(',').map(n=>parseInt(n));
+
+    if (x1 === x2) {
+        let x = x1;
+
+        for (let y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
+            if (typeof grid[y] === 'undefined') {
+                grid[y] = [];
+            }
+
+            grid[y][x] = typeof grid[y][x] !== 'undefined'
+                ? grid[y][x] + 1
+                : 1;
+        }
+    } else if (y1 === y2) {
+        let y = y1;
+
+        for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
+            if (typeof grid[y] === 'undefined') {
+                grid[y] = [];
+            }
+
+            grid[y][x] = typeof grid[y][x] !== 'undefined'
+                ? grid[y][x] + 1
+                : 1;
+        }
+    } else if (x1 < x2) {
+        let y = y1;
+
+        for (let x = x1; x <= x2; x++) {
+            if (typeof grid[y] === 'undefined') {
+                grid[y] = [];
+            }
+
+            grid[y][x] = typeof grid[y][x] !== 'undefined'
+                ? grid[y][x] + 1
+                : 1;
+
+            y = y1 < y2
+                ? y + 1
+                : y - 1;
+        }
+    } else {
+        let y = y1;
+
+        for (let x = x1; x >= x2; x--) {
+            if (typeof grid[y] === 'undefined') {
+                grid[y] = [];
+            }
+
+            grid[y][x] = typeof grid[y][x] !== 'undefined'
+                ? grid[y][x] + 1
+                : 1;
+
+            y = y1 < y2
+                ? y + 1
+                : y - 1;
+        }
+    }
+});
+
+let answer = 0;
+
+grid.forEach(function (row) {
+    const danger = row.filter(function (cell) {
+        return cell > 1;
+    });
+
+    answer += danger.length;
+});
+
+console.log(answer);
