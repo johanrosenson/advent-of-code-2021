3d43ac2e3e8a18c15d79884b8550ea7f2ec66809
Johan <johan@devlop.se>
2021-12-07 11:34:53 +0700

    refactor day6 part2

--- day_06/part2.js
+++ day_06/part2.js
@@ -2,36 +2,22 @@ require('../array');
 
 const fs = require('fs');
 
-const initial = fs.readFileSync('input.txt', 'utf8')
-    .split(',').map(v=>parseInt(v));
+const input = fs.readFileSync('input.txt', 'utf8')
+    .split(',')
+    .map(v => parseInt(v));
 
-let fishes = [];
-
-for (let i = 0; i < 9; i++) {
-    fishes[i] = initial.filter(function (fish) {
-        return fish === i;
-    }).length;
-}
+const fishes = range(0, 8)
+    .map(function (index) {
+        return input.filter(timer => timer === index).length;
+    });
 
 let days = 256;
 
-while (days > 0) {
-    let newFishes = [0, 0, 0, 0, 0, 0, 0, 0];
+while (days-- > 0) {
+    const fish = fishes.shift();
 
-    for (let i = 0; i < 9; i++) {
-        if (i === 0) {
-            newFishes[6] = fishes[i];
-            newFishes[8] = fishes[i];
-        } else {
-            newFishes[i-1] = newFishes[i-1] + fishes[i];
-        }
-    }
-
-    fishes = newFishes;
-
-    days--;
+    fishes.push(fish);
+    fishes[6] += fish;
 }
 
-console.log(
-    fishes.sum(),
-);
+console.log(fishes.sum());
