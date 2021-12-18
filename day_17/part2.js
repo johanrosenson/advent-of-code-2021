4ec4c35de63f57fce1a96c2091d5c8d41e844ed0
Johan <johan@devlop.se>
2021-12-18 15:33:16 +0700

    refactor d17p2

--- day_17/part2.js
+++ day_17/part2.js
@@ -4,7 +4,7 @@ console.time('part2');
 
 const fs = require('fs');
 
-const input = fs.readFileSync('example.txt', 'utf8').trim()
+const input = fs.readFileSync('input.txt', 'utf8').trim()
 
 const parseInput = function (input) {
     const [x, y] = input.replace(/^target area: /, '').split(', ');
@@ -56,37 +56,16 @@ const calculateXPosition = function (xv, steps) {
     return xPosition;
 };
 
-const validate = function (trajectories, expected) {
-    for (expectedTrajectory of expected) {
-        let found = false;
-        for (trajectory of trajectories) {
-            const key = trajectory[0] + ',' + trajectory[1];
-
-            if (key === expectedTrajectory) {
-                found = true;
-                break;
-            }
-        }
-
-        if (! found) {
-            return false;
-        }
-    }
-
-    return true;
-};
-
 const run = async function () {
     const verticalHitThreshold = target.y2;
     const verticalMissThreshold = target.y1 - 1;
 
     const xvMin = calculateMinXV();
     const xvMax = target.x2;
-
     const yvMin = Math.abs(target.y1) * -1;
     const yvMax = Math.abs(target.y1) - 1;
 
-    const trajectories = [];
+    let trajectories = 0;
     let iterations = 0;
 
     for (let yv = yvMin; yv <= yvMax; yv++) {
@@ -120,7 +99,7 @@ const run = async function () {
         }
 
         if (yMinSteps === null) {
-            break;
+            continue;
         }
 
         yMinSteps += roundtripSteps;
@@ -133,10 +112,7 @@ const run = async function () {
                 const xPosition = calculateXPosition(xv, steps);
 
                 if (xPosition >= target.x1 && xPosition <= target.x2) {
-                    trajectories.push([
-                        xv,
-                        yv,
-                    ]);
+                    trajectories++;
 
                     break;
                 }
@@ -144,29 +120,10 @@ const run = async function () {
         }
     }
 
-    const expected = 112;
-
-    console.log({trajectories});
-    console.log([trajectories.length, expected, trajectories.length === expected]);
-    console.log({iterations});
-
-    const validated = validate(
+    console.log({
         trajectories,
-        [
-            '23,-10', '25,-9', '27,-5', '29,-6', '22,-6', '21,-7', '9,0', '27,-7', '24,-5', '25,-7', '26,-6',
-            '25,-5', '6,8', '11,-2', '20,-5', '29,-10', '6,3', '28,-7', '8,0', '30,-6', '29,-8', '20,-10', '6,7',
-            '6,4', '6,1', '14,-4', '21,-6', '26,-10', '7,-1', '7,7', '8,-1', '21,-9', '6,2', '20,-7', '30,-10',
-            '14,-3', '20,-8', '13,-2', '7,3', '28,-8', '29,-9', '15,-3', '22,-5', '26,-8', '25,-8', '25,-6',
-            '15,-4', '9,-2', '15,-2', '12,-2', '28,-9', '12,-3', '24,-6', '23,-7', '25,-10', '7,8', '11,-3',
-            '26,-7', '7,1', '23,-9', '6,0', '22,-10', '27,-6', '8,1', '22,-8', '13,-4', '7,6', '28,-6', '11,-4',
-            '12,-4', '26,-9', '7,4', '24,-10', '23,-8', '30,-8', '7,0', '9,-1', '10,-1', '26,-5', '22,-9', '6,5',
-            '7,5', '23,-6', '28,-10', '10,-2', '11,-1', '20,-9', '14,-2', '29,-7', '13,-3', '23,-5', '24,-8',
-            '27,-9', '30,-7', '28,-5', '21,-10', '7,9', '6,6', '21,-5', '27,-10', '7,2', '30,-9', '21,-8',
-            '22,-7', '24,-9', '20,-6', '6,9', '29,-5', '8,-2', '27,-8', '30,-5', '24,-7',
-        ],
-    );
-
-    console.log({validated});
+        iterations,
+    });
 
     console.timeEnd('part2');
 };
